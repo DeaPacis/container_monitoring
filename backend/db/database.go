feaf321dcf998ed5b5abab1fb7a5179ea2045c49
nastya <120595306+DeaPacis@users.noreply.github.com>
2025-02-09 20:21:41 +0300

    feature(backend): add env database params

--- backend/db/database.go
+++ backend/db/database.go
@@ -4,23 +4,22 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/lib/pq"
-)
-
-const (
-	host     = "localhost"
-	port     = 5435
-	user     = "postgres"
-	password = "passPinger"
-	dbname   = "postgres"
 )
 
 var Db *sql.DB
 
 func InitDB() {
 	var err error
-	dbinfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+	host := os.Getenv("POSTGRES_HOST")
+	port := os.Getenv("POSTGRES_PORT")
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	dbname := os.Getenv("POSTGRES_DB")
+
+	dbinfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
 
 	Db, err = sql.Open("postgres", dbinfo)
